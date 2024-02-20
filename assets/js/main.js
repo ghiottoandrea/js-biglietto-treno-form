8f2fb4704d88e86bbaf06a660914e22a0448f182
ghiottoandrea <andreaghiotto9@gmail.com>
2024-02-20 17:54:47 +0100

    fix formula js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,44 @@
 
-const discount_underage = 0.2
-const discount_over = 0.4
-const price_km = 0.21
+btn.addEventListener("click", function () {
+    const user_name = getUsername();
+    const distance = getDistance();
+    const age = getAge();
 
+    getPrice(distance, age);
+});
 
-let finalPrice = document.getElementById("btn");
-finalPrice.addEventListener("click", getValue);
-finalPrice.addEventListener("click", getSelectedOption);
-finalPrice.addEventListener("click", getPrice);
-
-
-function getValue() {
-    const user_name = document.getElementById("user_name").value;
-    console.log(user_name);
+function getDistance() {
     const distance = document.getElementById("distance").value;
-    console.log(distance)
+    console.log(distance);
+    return distance;
 }
 
-function getSelectedOption() {
+function getUsername() {
+    const user_name = document.getElementById("user_name").value;
+    console.log(user_name);
+}
+
+function getAge() {
     const selectElement = document.getElementById("selectOptions");
     const age = selectElement.options[selectElement.selectedIndex].value;
     console.log(age);
+    return age;
 }
 
-function getPrice() {
+function getPrice(distance, age) {
+    const discount_underage = 0.2
+    const discount_over = 0.4
+    const price_km = 0.21
 
-    let price = (distance * price_km)
+    let price = distance * price_km;
 
-    if (age = "minor") {
-        let price = (price - price * discount_underage).toFixed(2);
-
-    } else if (age = "over") {
-        let price = (price - price * discount_over).toFixed(2);
-
+    if (age === "minor") {
+        finalPrice = (price - price * discount_underage).toFixed(2);
+    } else if (age === "over") {
+        finalPrice = (price - price * discount_over).toFixed(2);
     } else {
-        let price = price.toFixed(2);
-
+        finalPrice = price.toFixed(2);
     }
+
+    console.log(finalPrice);
 }
